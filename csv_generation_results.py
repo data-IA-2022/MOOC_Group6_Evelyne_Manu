9d54d86d6124ae2f5a6225df673820c1e9903605
Evelyne <evelyne.theodose@gmail.com>
2023-03-10 19:59:56 +0100

    livrables avec les commentaires

---
+++ csv_generation_results.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine, types
+import os, yaml # credentials:
+
+
+# Récup des info de connection
+with open('config_local.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+#print(config)
+
+cfg=config['PG']
+print(cfg)
+
+# Connection à BDD
+url = "{driver}://{user}:{password}@{host}/{database}".format(**cfg)
+print('URL', url)
+engine = create_engine(url)
+
+df = pd.read_sql("""SELECT username, course_id, grade FROM "Results";""", engine)
+
+#print(df)
+
+df.to_csv('results.csv', index=False)
+
+print('done')
